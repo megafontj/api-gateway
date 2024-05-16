0da00594d1265f4d28fcec28821b6ad2c09e058a
Safar Umarov <safarumarov711@gmail.com>
2024-05-16 23:18:57 +0700

    fixed names issue

--- config/microservices.php
+++ config/microservices.php
@@ -2,12 +2,12 @@
 
 return [
     'users_auth' => [
-        'base_uri' => env('USERS_AUTH_HOST') . '/api/'
+        'base_uri' => env('USERS_AUTH_SERVICE_HOST') . '/api/'
     ],
     'accounts' => [
-        'base_uri' => env('ACCOUNTS_AUTH_HOST') . '/api/',
+        'base_uri' => env('ACCOUNTS_SERVICE_HOST') . '/api/',
     ],
     'tweets' => [
-        'base_uri' => env('TWEETS_AUTH_HOST') . '/api/',
+        'base_uri' => env('TWEETS_SERVICE_HOST') . '/api/',
     ]
 ];
